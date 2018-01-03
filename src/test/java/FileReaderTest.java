46c7e381b9bb58d904db71a76a7e2e1968a487bb
Mathias Thomsen <=>
2018-01-03 15:46:40 +0100

    lesmallfix

--- src/test/java/FileReaderTest.java
+++ src/test/java/FileReaderTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import static groovy.util.GroovyTestCase.assertEquals;
 import static org.junit.Assert.*;
 
 public class FileReaderTest {
