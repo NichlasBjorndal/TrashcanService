defd91c844f2559c790af16ca59db5c1c23ecbd8
Nichlas M. Bjørndal <nichlasbj@gmail.com>
2018-01-03 10:14:35 +0100

    First Maven

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         // Prints "Hello, World" in the terminal window.
         System.out.println("Hello, World");
+        int i = 1;
     }
 }
