2f5cc56117d51c92f91811296285014638a2c139
Mathias Thomsen <=>
2018-01-03 15:35:51 +0100

    main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,13 @@
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        // Prints "Hello, World" in the terminal window.
-        System.out.println("Hello, World");
-        int i = 2;
+        CurrencyConverter cc = new CurrencyConverter();
+        ArrayList<String> contents = FileReader.getInput();
+        for (String s :
+                contents) {
+            System.out.println(cc.convert(s));
+        }
+
     }
 }
