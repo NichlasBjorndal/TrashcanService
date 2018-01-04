507f305a2de30ad9ce8b63490d50e4006d18a32c
Nichlas M. Bjørndal <nichlasbj@gmail.com>
2018-01-04 12:45:47 +0100

    Currency converter

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) {
         // Prints "Hello, World" in the terminal window.
         CurrencyConverter currencyConverter = new CurrencyConverter();
