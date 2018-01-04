373fdc9606817169b9070b15905805a9af6f4c24
Mathias Thomsen <=>
2018-01-04 14:15:14 +0100

    tests updated

--- src/main/java/CurrencyConverter.java
+++ src/main/java/CurrencyConverter.java
@@ -1,15 +1,40 @@
+import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.reflect.Array;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+
+import static java.nio.file.Files.newOutputStream;
+import static java.nio.file.Files.readAllLines;
+import static java.nio.file.Files.write;
 
 /**
  * Converts valuta to USD.
  */
 class CurrencyConverter {
+    private static final String CONVERSION_RATE_FILE_NAME = "ConvertionRates.txt";
 
     private HashMap<String,Double> conversionRates;
 
     CurrencyConverter() throws IOException {
-        conversionRates = FileIO.getConversionRates();
+        conversionRates = getConversionRates();
+    }
+
+    static HashMap<String, Double> getConversionRates() throws IOException {
+        HashMap<String, Double> conversionRates = new HashMap<>();
+
+        List<String> input = readAllLines(new File(CONVERSION_RATE_FILE_NAME).toPath());
+
+        for (String s : input) {
+            String[] rate = s.split(" ");
+
+            conversionRates.put(rate[0], Double.parseDouble(rate[1]));
+        }
+
+        return conversionRates;
     }
 
     /**
@@ -21,5 +46,22 @@ class CurrencyConverter {
         double conversionRate = conversionRates.get(currencyAmountPair[0]);
 
         return  conversionRate * Double.parseDouble( currencyAmountPair[1]);
+    }
+
+    void convertFile(String inputFileName, String outputFileName) throws IOException {
+        ArrayList<String> inputAmounts = (ArrayList<String>) readAllLines(new File(inputFileName).toPath());
+
+        //Create output file.
+        File file = new File(outputFileName);
+
+        OutputStream os = newOutputStream(file.toPath());
+        os.write(("").getBytes());
+
+        //Calculate conversions and write to output file.
+        for (String inputAmount : inputAmounts) {
+            double outputAmount = convert(inputAmount);
+
+            write(file.toPath(), (outputAmount + "\n").getBytes(), StandardOpenOption.APPEND);
+        }
     }
 }
--- src/test/java/CurrencyConverterTest.java
+++ src/test/java/CurrencyConverterTest.java
@@ -1,62 +1,74 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.OutputStream;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 
+import static java.nio.file.Files.newOutputStream;
+import static java.nio.file.Files.readAllLines;
+import static java.nio.file.Files.write;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class CurrencyConverterTest {
     private CurrencyConverter currencyConverter;
-    private FileIO fileIO;
 
-    private ArrayList<String> fileContents;
-
-    private Double[] expectedConversionResults;
     private double conversionResult;
 
     @Before
     public void setUp() throws Exception {
-        fileIO = new FileIO();
-
-        expectedConversionResults = new Double[]{1000*0.42,500*0.69,1284*0.42,9587*0.69,239*0.69};
-
         currencyConverter = new CurrencyConverter();
     }
 
     @Test
-    public void dkkConvertion() throws Exception {
-        fileContents = fileIO.getInput();
+    public void dkkConversion() throws Exception {
+        conversionResult = currencyConverter.convert("EUR 1000");
 
-        assertNotNull(fileContents);
-
-        conversionResult = currencyConverter.convert(fileContents.get(1));
-
-        assertEquals(expectedConversionResults[1],conversionResult,0.5);
+        assertEquals(1000*0.42,conversionResult,0.5);
     }
 
     @Test
-    public void eurConvertion() throws Exception {
-        fileContents = fileIO.getInput();
+    public void eurConversion() throws Exception {
+        conversionResult = currencyConverter.convert("DKK 500");
 
-        assertNotNull(fileContents);
-
-        conversionResult = currencyConverter.convert(fileContents.get(0));
-
-        assertEquals(expectedConversionResults[0],conversionResult,0.5);
+        assertEquals(500*0.69,conversionResult,0.5);
     }
 
     @Test
-    public void convertAll() throws Exception {
-        fileContents = fileIO.getInput();
+    public void convertFile() throws Exception {
+        File inputFile = new File("Input.txt");
 
-        assertNotNull(fileContents);
+        OutputStream os = newOutputStream(inputFile.toPath());
+        os.write(("").getBytes());
+
+        assertNotNull(inputFile);
+
+        Double[] expectedConversionResults = new Double[]{1000*0.42,500*0.69,420*0.42,230*0.69};
+
+        ArrayList<String> inputFileContents = new ArrayList<>();
+        inputFileContents.add("EUR 1000");
+        inputFileContents.add("DKK 500");
+        inputFileContents.add("EUR 420");
+        inputFileContents.add("DKK 230");
+
+        for (String amount : inputFileContents) {
+            write(inputFile.toPath(), (amount + "\n").getBytes(), StandardOpenOption.APPEND);
+        }
+
+        currencyConverter.convertFile("Input.txt","Output.txt");
+
+        File outputFile = new File("Output.txt");
+
+        ArrayList<String> resultingConversions = (ArrayList<String>) readAllLines(outputFile.toPath());
+
+        assertEquals(4,resultingConversions.size());
 
         int i = 0;
-        for (String s :
-                fileContents) {
-            conversionResult = currencyConverter.convert(s);
-            assertEquals(conversionResult,expectedConversionResults[i],0.5);
+        for (String conversionResult : resultingConversions) {
+            assertEquals(expectedConversionResults[i],Double.parseDouble(conversionResult),0.5);
             i++;
         }
     }
