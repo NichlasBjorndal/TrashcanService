f90291129ba8f9b307f29fc0721c4c7ed1f59259
Nichlas M. Bjørndal <nichlasbj@gmail.com>
2018-01-12 15:26:55 +0100

    Split user class into a customer and a merchant class

--- DTUPay/src/main/java/core/user/User.java
+++ DTUPay/src/main/java/core/user/User.java
@@ -1,11 +1,6 @@
 package core.user;
 
 
-import core.barcode.BarcodeGenerator;
-import core.barcode.BarcodeGeneratorInterface;
-import core.barcode.Model.Barcode;
-
-import java.io.IOException;
 import java.util.UUID;
 
 /**
@@ -13,40 +8,15 @@ import java.util.UUID;
  */
 public class User {
 
-    private Barcode barcode;
     private String name;
     private UUID userID;
-    private transient BarcodeGeneratorInterface barcodeGenerator;
-    private String cpr;
+    private Double accountBalance;
 
-    /**
-     * @param name defining the user's full name
-     * @param cpr defining the user's cpr-number
-     */
-    public User(String name, String cpr){
-        this.cpr = cpr;
-        this.barcodeGenerator = new BarcodeGenerator();
-        this.userID = UUID.randomUUID();
-        this.name = name;
-    }
 
     //Used for GSON
     public User(){
-        this.barcodeGenerator = new BarcodeGenerator();
         this.userID = UUID.randomUUID();
     }
-
-    /** requests barcode from barcode generator
-     * @throws IOException
-     */
-    public void requestBarcode() throws IOException {
-        barcode = barcodeGenerator.generateBarcode();
-    }
-
-    public Barcode getBarcode() {
-        return barcode;
-    }
-
 
     /**
      * @return the user's full name
@@ -62,17 +32,16 @@ public class User {
         return userID;
     }
 
-
-    public void setBarcodeGenerator(BarcodeGeneratorInterface barcodeGenerator) {
-        this.barcodeGenerator = barcodeGenerator;
+    public void setName(String name) {
+        this.name = name;
     }
 
+    public Double getAccountBalance() {
+        return accountBalance;
+    }
 
-    /**
-     * @return the user's cpr-number
-     */
-    public String getCpr() {
-        return cpr;
+    public void setAccountBalance(Double accountBalance) {
+        this.accountBalance = accountBalance;
     }
 }
 
