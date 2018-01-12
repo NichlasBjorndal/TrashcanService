50335b3cfb4984c0d592f35e1f5354ccd1a691e3
Nichlas M. Bjørndal <nichlasbj@gmail.com>
2018-01-12 13:16:03 +0100

    added new project structure

---
+++ SystemTest/src/test/java/myTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class myTest {
+
+    @Test
+    public void generateBarcode() {
+
+        assertTrue(true);
+    }
+}
