e6e2a20261d5637fa96cbc5e8e884f0d7ee37a03
Nichlas M. Bjørndal <nichlasbj@gmail.com>
2018-01-12 13:10:44 +0100

    cleaning out the old project

--- src/main/java/api/Application.java
+++
@@ -1,8 +0,0 @@
-package api;
-
-import javax.ws.rs.ApplicationPath;
-
-@ApplicationPath("/api")
-public class Application extends javax.ws.rs.core.Application {
-    //...
-}
--- src/main/java/dtu/ws/fastmoney/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://fastmoney.ws.dtu/")
-package dtu.ws.fastmoney;
