580602cce760e0d81ea4dcbef240a670b356af70
Mikkel Møller Brusen <Mikkel_mb@hotmail.com>
2018-01-15 09:52:14 +0100

    move tests to system tests

--- ClientSimulator/src/main/java/ClientSimulator.java
+++ ClientSimulator/src/main/java/ClientSimulator.java
@@ -1,19 +1,14 @@
-import core.user.Customer;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
-import mdb.utils.GsonWrapper;
 
 public class ClientSimulator {
-
 
     public static String endpoint = "http://159.89.18.108:8080/DTUPay-0.5/api";
 
     public ClientSimulator(){}
 
-
     public String createCustomer() throws UnirestException {
-
         //make json
         HttpResponse<String> r = Unirest.post(endpoint + "/customer").header("Content-Type", "application/json").body("{\"name\": \"mikkel\",\"cpr\": \"123\"}").asString();
         return r.getBody();
