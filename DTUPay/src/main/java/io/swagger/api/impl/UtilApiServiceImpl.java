e67b612fcc39e850b3709c7e4b02f03ef618b3b1
Nichlas M. Bjørndal <nichlasbj@gmail.com>
2018-01-15 15:08:58 +0100

    implemented flush

--- DTUPay/src/main/java/io/swagger/api/impl/UtilApiServiceImpl.java
+++ DTUPay/src/main/java/io/swagger/api/impl/UtilApiServiceImpl.java
@@ -7,6 +7,8 @@ import io.swagger.model.*;
 
 import java.util.List;
 import io.swagger.api.NotFoundException;
+import persistence.CustomerStore;
+import persistence.UserStore;
 
 import java.io.InputStream;
 
@@ -16,9 +18,9 @@ import javax.ws.rs.core.SecurityContext;
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaResteasyServerCodegen", date = "2018-01-15T13:52:27.201Z")
 public class UtilApiServiceImpl extends UtilApiService {
       @Override
-      public Response flushData(SecurityContext securityContext)
-      throws NotFoundException {
-      // do some magic!
-      return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+      public Response flushData(SecurityContext securityContext) throws NotFoundException {
+          CustomerStore.getInstance().clearStore();
+          UserStore.getInstance().clearStore();
+      return Response.ok().build();
   }
 }
